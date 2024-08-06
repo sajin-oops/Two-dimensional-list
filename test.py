a464cee2ebbef8f9956dd1670f1c425c71f3df1d
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-06 20:52:00 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+TN = ["chennai","coimbatore","salem"]
+KL = ["alapuzha","kochi"]
+AP = ["bangalore","mysore","karnataka"]
+india = [TN,KL,AP]
+print(india)
+
+'''
+OUTPUT 
+
+[['chennai', 'coimbatore', 'salem'], ['alapuzha', 'kochi'], ['bangalore', 'mysore', 'karnataka']]
+'''
